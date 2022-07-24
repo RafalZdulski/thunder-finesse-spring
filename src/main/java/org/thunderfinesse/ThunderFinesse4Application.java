df5d1b854e3724bc8ccde6fdbd67f6fc51deba91
R.Zdulski <r.p.zdulski@gmail.com>
2022-07-24 20:39:31 +0200

    init app

---
+++ src/main/java/org/thunderfinesse/ThunderFinesse4Application.java
@@ -0,0 +1,13 @@
+package org.thunderfinesse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderFinesse4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThunderFinesse4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/thunderfinesse/ThunderFinesse4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thunderfinesse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderFinesse4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
